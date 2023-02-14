c85e14b950189cafc0f1a5fd26319f83ccdddc60
hartkaymann <hartmannka80488@th-nuernberg.de>
2023-02-14 23:28:43 +0100

    added gitignore

--- bin/stonks.sh
+++ bin/stonks.sh
@@ -9,8 +9,45 @@
 #         Author: hartmannka80488@th-nuernberg.de
 #----------------------------------------------------
 
+function set_interval()
+{
+  arr=("1" "5" "15" "30" "45" "60")
+  if [[ " ${arr[*]} " =~ " ${1} " ]]; then
+    interval="${1}min"
+  else
+    exit 0;
+  fi
+}
+
 # Clear debug file
 true > debug.txt
+
+# Script parameters
+version=0.0.1
+draw=1
+
+# API parameters
+func=TIME_SERIES_INTRADAY
+interval=1min
+apikey=AV6H9H7PL4L284AY
+datatype=csv
+
+while [[ "${1-}" =~ ^- ]]; do
+  case "$1" in
+    -V | --version )  echo "Version $version" ; exit 0 ;;
+    -s | --symbol ) shift ; symbol=$1 ;; 
+    -i | --interval ) shift ; set_interval "$1" ;;
+    -b | --brief ) draw=0 ;;
+    --) shift ; break ;;
+    *)  echo "Invalid option '$1'" >&2 ; exit 1 ;;
+  esac
+  shift
+done
+
+if [ -z "$symbol" ]; then
+        echo 'Missing -s' >&2
+        exit 1
+fi
 
 # Find max number in array
 function max()
@@ -120,39 +157,22 @@ function draw_stock()
 
 
   # Figure out positions and draw 
-  lastposy=0
-  sym="-"
   for ((i=1; i<=$1; i++)); do
     # Index to look up value
-    idx=$(map $i 1 "$1" 1 $(( ${#timestamp[@]} - 1 )) | cut -d, -f1)
+    idx=$(map $i "$1" 1 1 $(( ${#timestamp[@]} - 1 )) | cut -d, -f1)
     val=${close[$idx]}
 
     # Y-Position of value in coordinate system
     posy=$(map "$val" "$ymin" "$ymax" $yo $(( yo - $2 )) | cut -d, -f1)
 
     repeat_vertical $((yo-1)) $((xo+i)) $((yo-posy)) "*" 
-
-  : '
-    # Get symbol to draw by comparing to last position
-    if [[ "$lastposy" -gt "$posy" ]]; then
-      sym="/"
-    elif [[ "$lastposy" -lt "$posy" ]]; then
-      sym="\\"
-    else
-      sym="-"
-    fi
-    lastposy=$posy
-    # Mark actual position in coordinate system
-    tput cup "$posy" $((xo+i))
-    printf "%s" "$sym"
-  '
   done
 
   return 0
 }
 
 
-# TODO: convert to generic key event handler
+# TODO: maybe convert to generic key event handler
 # Infinite loop that stops when specified key is presse, might just be tempporary solution for debugging
 # Converts inputs to lower case letters before matching, so parameters should be lower case as well
 # @param: key 
@@ -160,7 +180,7 @@ function exit_loop()
 {
   while true; do
   tput cup "$h" 0
-    printf "Press %s to exit \t\t: " "$input"
+    printf "Press %s to exit \t\t: " "$1"
     read -n1 -r input
     if [[ ${input,,} = "$1" ]] 
         then break 
@@ -182,11 +202,6 @@ qw=$((w/4))
 qh=$((h/4))
 
 # Get stock data from Alpha Vantage API and save to csv
-func=TIME_SERIES_INTRADAY
-symbol=IBM
-interval=1min
-apikey=AV6H9H7PL4L284AY
-datatype=csv
 url="https://www.alphavantage.co/query?function=$func&symbol=$symbol&interval=$interval&apikey=$apikey&datatype=$datatype"
 curl -o stock.csv "$url"
 
@@ -208,12 +223,17 @@ tput ed
 # Print absolute values on right
 maxmax=$(max "${high[@]:1}")
 minmin=$(min "${low[@]:1}")
-draw_value_box $((w-15)) $((qh)) Low "$minmin"
-draw_value_box $((w-15)) $((qh*2)) High "$maxmax"
-draw_value_box $((w-15)) $((qh*3)) Close "${close[1]}"
 
-# Draw axes
-draw_stock "$((qw*3))" "$((qh*3))" 
+if [[ $draw == 1 ]]; then
+  draw_value_box $((w-15)) $((qh)) Low "$minmin"
+  draw_value_box $((w-15)) $((qh*2)) High "$maxmax"
+  draw_value_box $((w-15)) $((qh*3)) Close "${close[1]}"
+  draw_stock "$((qw*3))" "$((qh*3))" 
+else 
+  printf "Low: %s\n" "$minmin"
+  printf "Hight: %s\n" "$maxmax"
+  printf "Close: %s\n" "${close[1]}"
+fi
 
 # Start the exit loop
 exit_loop q
